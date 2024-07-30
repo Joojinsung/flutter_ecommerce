9dc14597405178b34d217eb950ea849dee9309c1
Joojinsung <dev.jinsung1017@gmail.com>
2024-07-30 14:31:01 +0900

    appbar, bottmbar 작업

--- lib/presentation/main/main_screen.dart
+++ lib/presentation/main/main_screen.dart
@@ -1,22 +1,91 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import '../../core/theme/constant/app_colors.dart';
 import '../../core/theme/constant/app_icons.dart';
 
-
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedSegment = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("tabBar"),
-        leading: IconButton(
-          onPressed:(){}, icon: SvgPicture.asset(AppIcons.mainLogo),),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(44),
+        child: Container(
+          color: Theme.of(context).colorScheme.primary,
+          padding: const EdgeInsets.symmetric(vertical: 6.0, horizontal: 8),
+          child: AppBar(
+            backgroundColor: Colors.transparent,
+            centerTitle: true,
+            leadingWidth: 86,
+            leading: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SvgPicture.asset(AppIcons.mainLogo),
+            ),
+            title: Text(
+              "Main Screen",
+              style: TextStyle(
+                color: AppColors.onPrimary,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: SvgPicture.asset(
+                  colorFilter: ColorFilter.mode(
+                      Theme.of(context).colorScheme.background,
+                      BlendMode.srcIn),
+                  AppIcons.location,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: SvgPicture.asset(
+                  colorFilter: ColorFilter.mode(
+                      Theme.of(context).colorScheme.background,
+                      BlendMode.srcIn),
+                  AppIcons.cart,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
       body: Center(child: Text("main screen")),
+
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(AppIcons.navHome),
+            label: "navHome",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(AppIcons.navCategory),
+            label: "navCategory",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(AppIcons.navSearch),
+            label: "navSearch",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(AppIcons.navUser),
+            label: "navUser",
+          ),
+        ],
+      ),
     );
   }
 }
